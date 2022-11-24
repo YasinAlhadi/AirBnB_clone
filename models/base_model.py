91a894f71a94d4b12e684d9a4f053394f6e9f227
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-11-24 16:37:22 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,25 @@
 import uuid
 import json
 import datetime
+import os
 
 
 class BaseModel:
     """define Base class"""
 
-    def __init__(self):
-        """initailize instances"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *arg, **kwargs):
+        """initailize methods"""
+        if kwargs:
+            for name, value in kwargs.items():
+                if name != '__class__':
+                    if name == 'created_at' or name == 'updated_at':
+                        value = datetime.datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, name, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """print class information"""
