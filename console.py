3fb021328fc22feaba895afc81f55bb0c822a0f4
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-11-27 12:29:04 +0000

    Console 1.0

--- console.py
+++ console.py
@@ -8,11 +8,17 @@ import sys
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
-    classes = ['BaseModel', 'User']
+    classes = [
+            'BaseModel', 'User', 'Place', 'State', 'City', 'Amenity', 'Review']
     methods = ['all', 'show', 'update', 'destroy']
     prompt = '(hbnb) '
 
